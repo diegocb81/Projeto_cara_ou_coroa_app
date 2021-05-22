b9e6e9918dcbfe5036c468eb966cb4d224240755
Diego Barcellos <diegocb81@gmail.com>
2021-05-22 19:26:27 -0300

    Configurando a interface

---
+++ app/src/main/java/com/cursoandroid/projeto_cara_ou_coroa_app/ResultadoActivity.java
@@ -0,0 +1,14 @@
+package com.cursoandroid.projeto_cara_ou_coroa_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+    }
+}
